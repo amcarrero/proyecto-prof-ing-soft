db7e0b92658746ea6b836f9ff56620743c717896
Adrián Mora <adrian.mcarrero@gmail.com>
2020-12-05 16:24:43 +0100

    Docker added

--- Proyecto/src/main/java/com/PIngS/Proyecto/ProyectoApplication.java
+++ Proyecto/src/main/java/com/PIngS/Proyecto/ProyectoApplication.java
@@ -2,12 +2,29 @@ package com.PIngS.Proyecto;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoClientFactoryBean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ProyectoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProyectoApplication.class, args);
 	}
+	
+	 @GetMapping("/hello")
+	 public String hello(@RequestParam(value = "name", defaultValue = "World") String name) {
+	 return String.format("Hello %s!", name);
+	 }
+	 
+     public @Bean MongoClientFactoryBean mongo() {
+         MongoClientFactoryBean mongo = new MongoClientFactoryBean();
+         mongo.setHost("mongo");
+         return mongo;
+    }
 
 }
--- Proyecto/src/main/java/com/PIngS/Proyecto/controllers/controllerWebPage.java
+++ Proyecto/src/main/java/com/PIngS/Proyecto/controllers/controllerWebPage.java
@@ -1,10 +1,15 @@
 package com.PIngS.Proyecto.controllers;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping(value = "/api")
 public class controllerWebPage {
 
+	@GetMapping
+	public String HelloWorld() {
+		return "Hello world!";
+	}
 }
