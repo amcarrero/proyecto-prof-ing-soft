e1910cd0d4c653e50c4c9fcd45dcb642e6741363
Javier Cabrera <cabrera.javier23@gmail.com>
2020-12-05 19:39:00 +0100

    email sender created

---
+++ Proyecto/src/main/java/com/ing/soft/proyecto/email/EmailSender.java
@@ -0,0 +1,47 @@
+package com.ing.soft.proyecto.email;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+public class EmailSender {
+
+    public EmailSender() {
+    }
+
+    public void enviarConGMail(String destinatario, String asunto, String cuerpo) {
+
+        String remitente = "proyecto.prof.ing.soft.ACJAM";  //Para la dirección proyecto.prof.ing.soft.ACJAM@gmail.com
+
+        Properties props = System.getProperties();
+        props.put("mail.smtp.host", "smtp.gmail.com");  //El servidor SMTP de Google
+        props.put("mail.smtp.user", remitente);
+        props.put("mail.smtp.clave", "acjam1234");    //La clave de la cuenta
+        props.put("mail.smtp.auth", "true");    //Usar autenticación mediante usuario y clave
+        props.put("mail.smtp.starttls.enable", "true"); //Para conectar de manera segura al servidor SMTP
+        props.put("mail.smtp.port", "587"); //El puerto SMTP seguro de Google
+
+        Session session = Session.getDefaultInstance(props);
+        MimeMessage message = new MimeMessage(session);
+
+        try {
+            message.setFrom(new InternetAddress(remitente));
+            message.addRecipient(Message.RecipientType.TO, new InternetAddress(destinatario));   //Se podrían añadir varios de la misma manera
+            message.setSubject(asunto);
+            message.setText(cuerpo);
+            Transport transport = session.getTransport("smtp");
+            transport.connect("smtp.gmail.com", remitente, "acjam1234");
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+        }
+        catch (MessagingException me) {
+            me.printStackTrace();   //Si se produce un error
+        }
+    }
+
+
+}
