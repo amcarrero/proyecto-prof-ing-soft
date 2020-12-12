ec92a66b962c3055958193f2bd4f24986d2c81ed
adrian <cabrera23.adrian@gmail.com>
2020-12-12 18:30:26 +0100

    Tests

--- Proyecto/src/test/java/com/ing/soft/proyecto/controller/ControllerTest2.java
+++ Proyecto/src/test/java/com/ing/soft/proyecto/controller/ControllerTest2.java
@@ -4,6 +4,11 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.mail.MessagingException;
+
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -15,7 +20,7 @@ import com.ing.soft.proyecto.repositories.UserPetitionsRepo;
 class ControllerTest2 {
 
 	@Test
-	void test() {
+	void testController1() throws MessagingException {
 		Controller c = new Controller();
 		UserPetitionsRepo rep = mock(UserPetitionsRepo.class);
 		when(rep.insert(Mockito.anyIterable())).thenReturn(null);
@@ -25,6 +30,17 @@ class ControllerTest2 {
 		petition.id="1";
 		petition.url="http://circo.com";
 		c.insertPetition(petition);
+	}
+	
+	@Test
+	void testController2() {
+		Controller c = new Controller();
+		UserPetitionsRepo rep = mock(UserPetitionsRepo.class);
+		List<UsersPetitions> value = new ArrayList<UsersPetitions>();
+		when(rep.findAll()).thenReturn(value );
+		ReflectionTestUtils.setField(c,  "repository",rep);
+		c.getPetitions();
+
 	}
 
 }
