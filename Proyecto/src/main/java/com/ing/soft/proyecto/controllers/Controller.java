f3f227af5875b34f292bfcd86328aa896cf41ee2
Javier Cabrera <cabrera.javier23@gmail.com>
2020-12-13 12:22:15 +0100

    check if petition already exist

--- Proyecto/src/main/java/com/ing/soft/proyecto/controllers/Controller.java
+++ Proyecto/src/main/java/com/ing/soft/proyecto/controllers/Controller.java
@@ -3,6 +3,7 @@ package com.ing.soft.proyecto.controllers;
 import java.io.IOException;
 import java.util.List;
 
+import com.ing.soft.proyecto.checkersDatabase.CheckExistPetition;
 import com.ing.soft.proyecto.model.UrlContent;
 import com.ing.soft.proyecto.repositories.UrlHashRepo;
 import com.ing.soft.proyecto.services.EmailSender;
@@ -36,16 +37,21 @@ public class Controller {
 
 	@PostMapping(consumes = "Application/Json")
 	public UsersPetitions insertPetition(@RequestBody UsersPetitions petition) throws IOException, MessagingException {
-		//insert user petition in database
-		repository.insert(petition);
-		// send email to confirm
-		EmailSender emailSender = new EmailSender();
-		emailSender.enviarConGMail(petition.mail,"UrlComparator: Url recibida","La url "+petition.url+" va a ser monitorizada.\nUn saludo:3");
-		//insert the current content of url in database
-		GetContentUrl urlContent = new GetContentUrl();
-		String content = urlContent.getContentUrl(petition.getUrl());
-		UrlContent url = new UrlContent(petition.url, content.hashCode());
-		urlRepository.insert(url);
+		//se comprueba si el usuario ha solicitado ya esta peticion
+		CheckExistPetition ch = new CheckExistPetition();
+		if(!ch.checkIfAlreadyExist(petition.url,petition.mail)){
+			//insert user petition in database
+			repository.insert(petition);
+			// send email to confirm
+			EmailSender emailSender = new EmailSender();
+			emailSender.enviarConGMail(petition.mail,"UrlComparator: Url recibida","La url "+petition.url+" va a ser monitorizada.\nUn saludo:3");
+			//insert the current content of url in database
+			GetContentUrl urlContent = new GetContentUrl();
+			String content = urlContent.getContentUrl(petition.getUrl());
+			UrlContent url = new UrlContent(petition.url, content.hashCode());
+			urlRepository.insert(url);
+		}
+
 		return petition;
 	}
 
